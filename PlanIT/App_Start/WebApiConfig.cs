5a17ba728390bc89d2363014eb201641ac932e4a
Mindaugas <mynde101@gmail.com>
2019-12-05 15:59:10 +0000

    Swappies

--- PlanIT/App_Start/WebApiConfig.cs
+++ PlanIT/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace PlanIT
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
