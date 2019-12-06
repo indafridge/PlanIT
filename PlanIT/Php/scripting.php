b50d3219469ba2ed832a2bcd9c5a0d7092900bc4
Byrnzee <byrneciaran11@gmail.com>
2019-12-06 15:06:15 +0000

    php file..login changes

--- PlanIT/Php/scripting.php
+++ PlanIT/Php/scripting.php
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+<?php
 
-namespace PlanIT.Php
-{
-	public class scripting
-	{
-	}
+$servername = "planitserver.database.windows.net";
+$username = "planit";
+$password = "Password123";
+$dbname = "PlanITdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT id, email, password FROM login.Table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. $row["email"]. $row["password"]. br>";
+    }
+} else {
+    echo '0 results';
+}
+$conn->close();
+
+?>
