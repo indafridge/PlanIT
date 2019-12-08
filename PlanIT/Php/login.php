1510088369af91bdb0ff671df53151b1ce292aae
indafridge <x17491304@student.ncirl.ie>
2019-12-08 15:21:24 +0000

    changed login file type

--- PlanIT/Php/login.php
+++ PlanIT/Php/login.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. $row["email"]. $row["password"]. br>";
+        echo "id: " . $row["id"]. $row["email"]. $row["password"]. br>;
     }
 } else {
     echo '0 results';
