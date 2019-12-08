00a6ca7e23ffeee35fa6b58872e544ae140fa572
indafridge <x17491304@student.ncirl.ie>
2019-12-08 15:59:17 +0000

    work on login

--- PlanIT/Php/scripting.php
+++ PlanIT/Php/scripting.php
@@ -12,13 +12,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, email, password FROM login.Table";
+$sql = "SELECT id, email, password FROM tblRegistration";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. $row["email"]. $row["password"]. br>;
+        echo "id: " . $row["id"]. $row["email"]. $row["password"]. "br">;
     }
 } else {
     echo '0 results';
